a9f301fd51602afa3fa1509a786d3e087a6806f1
DCNick3 <nikita6@bk.ru>
2017-04-17 20:05:53 +0300

    Documentation update, created Protocol documentation file S4@TKseniya

--- ConvertDoc.sh
+++ ConvertDoc.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 pandoc MineBotGameSpec.md -o MineBotGameSpec.html
 sed -i '1s/^/<meta charset="UTF-8"> <style> table, th, td { border: 1px solid black; } .caption { font-style: italic; } <\/style>\n/' MineBotGameSpec.html
+pandoc MineBotGameProtocol.md -o MineBotGameProtocol.html
+sed -i '1s/^/<meta charset="UTF-8"> <style> table, th, td { border: 1px solid black; } .caption { font-style: italic; } <\/style>\n/' MineBotGameProtocol.html
