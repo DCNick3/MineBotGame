57418db8bee01b69d722fc5df53f5795e65bc6c1
TKseniya <tubaltseva.k@gmail.com>
2017-04-25 15:23:04 +0300

    Наброски интерфейсов и enums

---
+++ MineBotGame/Enums.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MineBotGame
+{
+    enum BuildingType
+    {
+        None = 0,
+        Base = 1,
+        Storage = 2,
+        ModuleFactory = 3,
+        Workshop = 4
+    }
+
+    enum ResourceType
+    {
+        None = 0,
+        Iron = 1,
+        Silicon = 2,
+        Uranium = 3,
+        Quartz = 4
+    }
+}
